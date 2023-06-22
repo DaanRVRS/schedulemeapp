ee6c11499d0f90e2a8c9f0f87511249050c00358
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2023-06-22 19:18:27 +0200

    first commit

---
+++ Models/Staff.cs
@@ -0,0 +1,10 @@
+﻿namespace Models;
+
+public class Staff
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Rank { get; set; } = string.Empty;
+}
